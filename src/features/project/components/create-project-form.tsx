07d4671177579b6cdd7059cbe40b629ffd833fd6
sajidctg1 <sajid.ctg03@gmail.com>
2025-06-01 12:30:35 +0600

    fix: fix create-project-form

--- src/features/project/components/create-project-form.tsx
+++ src/features/project/components/create-project-form.tsx
@@ -1,36 +1,42 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 import { FormInput } from "~/components/form/form-input";
 import { GenericForm } from "~/components/form/generic-form";
 import { ButtonLoading } from "~/components/ui-ext/button-loading";
+import { useWorkspaceId } from "~/features/workspace/hooks/use-workspace-id";
 import { api } from "~/trpc/react";
 
 import { type CreateProjectPayload, createProjectSchema } from "../schemas";
 
 export const CreateProjectForm = () => {
-  const router = useRouter();
+  const utils = api.useUtils();
+  const workspaceId = useWorkspaceId();
   const { mutate, isPending } = api.project.create.useMutation();
 
   const form = useForm<CreateProjectPayload>({
     resolver: zodResolver(createProjectSchema),
-    defaultValues: { name: "", image: "", workspaceId: "" },
+    defaultValues: { name: "", desc: "", workspaceId: "" },
   });
 
   const handleSubmit = (data: CreateProjectPayload) => {
-    mutate(data, {
-      onSuccess: () => {},
-    });
+    mutate(
+      { ...data, workspaceId },
+      {
+        onSuccess: () => {
+          utils.project.list.invalidate();
+        },
+      }
+    );
   };
 
   return (
     <GenericForm {...form} onSubmit={handleSubmit}>
       <FormInput name="name" />
       <FormInput name="desc" type="textarea" />
-      <ButtonLoading loading={isPending} className="w-full">
+      <ButtonLoading disabled={!form.formState.isDirty} loading={isPending}>
         create
       </ButtonLoading>
     </GenericForm>
